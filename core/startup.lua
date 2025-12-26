4c46d43a702ca38dfd47b38189a3e723845f8102
Brandy <brandy@lovegrove.co.nz>
2025-12-26 17:45:12 +1300

    fixed startup script call

--- core/startup.lua
+++ core/startup.lua
@@ -9,7 +9,7 @@ local cfg = require("core.config")
 -- download raw http/s response from web
 local download = require("core.download")
 local parse = require("core.parse")
-local stringtools = require("core.stringtools")
+local stringtools = require("core.string")
 
 -- acquire library files
 download.gitfolder(cfg.remote_paths.lib, "/lib")
